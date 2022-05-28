3fc7bfb754327a9b861b580e528e727965a72f4b
RaquelG18 <orgarces@espe.edu.ec>
2022-05-27 20:44:31 -0500

    Update

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 # Se importan las librerias necesarias para trabajar con el framwork Flask
 from flask import Flask, render_template
 
-
-# Se crea la aplicación, y se pasa como parametro el nombre de la aplicacion y el nombre de la carpeta donde se encuentra el archivo html
+# Se crea la aplicación con parámetro el nombre de la aplicación y el nombre de la carpeta donde se encuentra el archivo html
 app = Flask(__name__, template_folder='templates')
 
-# Se crea la ruta para la pagina principal o index
+# Se crea la ruta (/) 
 @app.route('/')
-def index():
+#Se define la función de nombre principal para llamar el archivo index.html
+def principal():
     # Retorna el archivo index.html o pag principal
     return render_template('index.html')
 
 
-# Ejecutar la aplicación en modo debug e
+# Menú para ejecutar la aplicación
 if __name__ == '__main__':
     app.run(debug=True)
